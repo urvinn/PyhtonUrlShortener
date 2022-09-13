897a4e911140c6611457766ff7185a80734fa8a7
Vinn <80805512+urvinn@users.noreply.github.com>
2022-09-13 09:46:53 +0700

    Auto Open Question If u using Windows

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import platform
 import json
 
 ###############################
@@ -9,9 +10,12 @@ import json
 # Free to edit without 
 # removing the developer name !
 ###############################
-
+operating = platform.system() 
 os.system('cls||clear')
-url_header = "=======================\n Simple URL Shortener\n   Created By Vinn\n=======================\n"
+url_header = """_  _ ____ _    ____ _  _ ____ ____ ___ ____ _  _ ____ ____ 
+|  | |__/ |    [__  |__| |  | |__/  |  |___ |\ | |___ |__/ 
+|__| |  \ |___ ___] |  | |__| |  \  |  |___ | \| |___ |  \ 
+================== [ Created By Vinn ]=======================\n\n"""
 print(url_header)
 url = input("Masukan Url Asli : ")
 os.system('cls||clear')
@@ -19,7 +23,7 @@ print(url_header)
 customnya = input("Masukan Custom URL\n(OPTIONAL - Press Enter To Skip) : ")
 os.system('cls||clear')
 print(url_header)
-print("Loading Data...")
+print("Loading Data...\n")
 payload = {
   'url': url,
   'costum': customnya
@@ -37,5 +41,15 @@ if status == "True":
     print(f"Short Links  : https://1nz.me/{data['result']['id']}")
     print(f"Delete Links : https://1nz.me/{data['result']['delete']}")
     print("\n======================\n")
-else:
-    print("\n======================\n")
+    if operating == "Windows":
+        tanya = input("Apakah Anda ingin Membuka URL (Y/N) ? ")
+        if tanya == "Y":
+            os.system(f"start https://1nz.me/{data['result']['id']}")
+        else:
+            os.system('cls||clear')
+        if tanya == "y":
+            os.system(f"start https://1nz.me/{data['result']['id']}")
+        else:
+            os.system('cls||clear')
+            print("\nThanks You\n")
+            print("\n======================\n")
